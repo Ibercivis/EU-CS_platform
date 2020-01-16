2ebce419e27da64b64048b27cd31a4753f50f4b9
unknown <vval@bifi.es>
2020-01-16 13:01:05 +0100

    Added more metadata fields to Project model

--- src/projects/models.py
+++ src/projects/models.py
@@ -2,13 +2,34 @@ from django.db import models
 from django.conf import settings
 
 
-class Project(models.Model):
-    name = models.CharField(max_length=100)
+class Project(models.Model):    
     category =  models.CharField(max_length=200)
-    url = models.CharField(max_length=200)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #Database information
+    dateCreated = models.DateTimeField('Created date', auto_now=True)
+    dateUpdated = models.DateTimeField('Updated date', auto_now=True)
+    origin = models.CharField(max_length=100)
+    #Basic Project Information
+    name = models.CharField(max_length=100)
+    aim = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    keywords = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
     start_date = models.DateTimeField('Start date')
     end_date = models.DateTimeField('End date')
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=100)
+    #Images and communications
+    url = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    imageCredit = models.CharField(max_length=200)
+    #Geography
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    #Personal and Organizational Affiliates
+    host = models.CharField(max_length=100)
+    #Supplementary information for Citizen Science
+    howToParticipate = models.CharField(max_length=300)
+    equipment = models.CharField(max_length=200)
     def __str__(self):        
         return f'{self.name}'
 
