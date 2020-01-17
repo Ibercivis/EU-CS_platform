e1613fbde10772c1b648469d07aac5a923b0873e
unknown <vval@bifi.es>
2020-01-17 09:08:55 +0100

    Admin panel with projects and documents

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project, Category
 
 # Register your models here.
+class ProjectA(admin.ModelAdmin):
+    list_filter = ('creator',)
+
+admin.site.register(Project, ProjectA)
+admin.site.register(Category)
