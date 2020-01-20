245fe278c5426e2c332ce3055258cd819c3a481f
unknown <vval@bifi.es>
2020-01-20 10:24:26 +0100

    Document model updated with more metadata fields

--- src/documents/forms.py
+++ src/documents/forms.py
@@ -7,27 +7,15 @@ from django.forms import ModelForm
 
 
 class DocumentForm(forms.ModelForm):
-    '''
-        document_name = forms.CharField(max_length=100)
-        author = forms.CharField(max_length=100)
-        description = forms.CharField(max_length=100)
-        url = forms.CharField(max_length=200)
-        document = forms.FileField()
-    '''
+
     class Meta:
         model = Document
-        fields = ["name", "author", "description", "url"]
+        fields = ["name", "author", "about", "abstract", "url", "audience",
+         "inLanguage", "keywords", "license", "publisher"]
         
 
     def save(self, args):
-        publication_date = datetime.now()
-        '''
-        print(self.data['document'])
-        doc = Document(name = self.data['name'], url = self.data['url'], 
-                         author = self.data['author'], description = self.data['description'], 
-                         datePublished = publication_date, document='1')
-        doc.save()
-        '''
+        publication_date = datetime.now()        
         doc = super(DocumentForm, self).save(commit=False)
         doc.datePublished = publication_date
         doc.save()
--- src/documents/models.py
+++ src/documents/models.py
@@ -4,10 +4,17 @@ from django.db import models
 class Document(models.Model):
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    description = models.CharField(max_length=300)
+    about  = models.CharField(max_length=200)
+    abstract = models.CharField(max_length=300)
+    aggregateRating = models.CharField(max_length=100)
+    audience = models.CharField(max_length=100)
     datePublished = models.DateTimeField('Date Published')
-    #document = models.FileField(upload_to='documents/')
+    inLanguage = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=100)
+    license =  models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
