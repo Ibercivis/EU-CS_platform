f9303aebc9a11886a91a41a38d2ac96463e4201e
unknown <vval@bifi.es>
2020-01-22 12:35:19 +0100

    Add document edition page and document detail page

--- src/documents/urls.py
+++ src/documents/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('documents', views.documents, name='documents'),
+    path('document/<int:pk>', views.document, name='document'),
     path('clearfilters_documents', views.clearFilters, name='clearfilters_documents'),
     path('new_document', views.new_document, name='new_document'),
+    path('editDocument/<int:pk>', views.editDocument, name='editDocument'),
+    #path('deleteProject/<int:pk>', views.deleteProject, name='deleteProject'),
 ]
 
