e8f6d7103f23afe5f9cb94f52108c72b32c1073b
unknown <vval@bifi.es>
2020-01-23 10:00:42 +0100

    Clean and foprmatted code

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.conf import settings
 
-
 class Project(models.Model):    
     category =  models.CharField(max_length=200)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
