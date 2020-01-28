a96509e17dc500b3de7239603a98010bb9a0b523
unknown <vval@bifi.es>
2020-01-28 15:29:07 +0100

    Update topic selector in project management admin page

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from .models import Project, Category, Status
+from django import forms
+from django_select2.forms import Select2MultipleWidget
 
-# Register your models here.
+
+class ProjectFormA(forms.ModelForm):
+    topic = forms.ModelMultipleChoiceField(queryset=Category.objects.all(), widget=Select2MultipleWidget, required=False)
+    def __init__(self, *args, **kwargs):
+        super(ProjectFormA, self).__init__(*args, **kwargs)
+        obj = kwargs.get('instance')
+        if obj:
+            initial = [i for i in obj.topic.split('#')]
+            self.initial['topic'] = initial
+    class Meta:
+        model = Project
+        exclude = ('origin',)
+
+
 class ProjectA(admin.ModelAdmin):
     list_filter = ('creator', 'status', )
+    form = ProjectFormA
+
 
 admin.site.register(Project, ProjectA)
 admin.site.register(Category)
