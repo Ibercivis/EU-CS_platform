8d04f09eb6a7a706619fc8ec4431ae4f3843b884
unknown <vval@bifi.es>
2020-02-25 17:00:30 +0100

    New fields for user profile

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -5,6 +5,11 @@ from django.db import models
 from django.conf import settings
 
 
+class InterestArea(models.Model):
+    interestArea = models.TextField()
+    def __str__(self):        
+        return f'{self.interestArea}'
+
 class BaseProfile(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True
@@ -15,7 +20,12 @@ class BaseProfile(models.Model):
     picture = models.ImageField(
         "Profile picture", upload_to="profile_pics/%Y-%m-%d/", null=True, blank=True
     )
-    bio = models.CharField("Short Bio", max_length=200, blank=True, null=True)
+    title = models.CharField("Title", max_length=200, blank=True, null=True)
+    bio = models.CharField("Short Bio and disciplinary background", max_length=400, blank=True, null=True)
+    institution = models.CharField("Institution", max_length=200, blank=True, null=True)
+    interestAreas = models.ManyToManyField(InterestArea, blank=True)
+    latitude = models.DecimalField(max_digits=9,decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9,decimal_places=6, blank=True, null=True)
     email_verified = models.BooleanField("Email verified", default=False)
 
     class Meta:
