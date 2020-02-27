dba992df54fb5bf4fe6763bc9976ea53f008afbe
unknown <vval@bifi.es>
2020-02-27 11:34:44 +0100

    Added SavedResources model

--- src/resources/admin.py
+++ src/resources/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Resource, ResourceGroup, ResourcesGrouped, Category, FeaturedResources
+from .models import Resource, ResourceGroup, ResourcesGrouped, Category, FeaturedResources, SavedResources
 # Register your models here.
 
 class ResourcesGroupedAdmin(admin.ModelAdmin):
@@ -16,3 +16,4 @@ admin.site.register(Category, CategoryGroupedAdmin)
 admin.site.register(ResourceGroup)
 admin.site.register(ResourcesGrouped, ResourcesGroupedAdmin)
 admin.site.register(FeaturedResources)
+admin.site.register(SavedResources)
