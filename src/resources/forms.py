cdd9ed04ba789de37f281d380e3e3fbf7e8633ff
unknown <vval@bifi.es>
2020-03-04 11:59:54 +0100

    Added image to resources and crop functionality

--- src/resources/forms.py
+++ src/resources/forms.py
@@ -7,6 +7,7 @@ from datetime import datetime, date
 from django.forms import ModelForm
 from django_select2.forms import Select2MultipleWidget
 from authors.models import Author
+from PIL import Image
 
 class ResourceForm(forms.ModelForm):
     abstract = forms.CharField(widget=forms.Textarea(attrs={"rows":5, "cols":20}), max_length=1000)   
@@ -20,19 +21,22 @@ class ResourceForm(forms.ModelForm):
     authors = forms.MultipleChoiceField(choices=(), widget=Select2MultipleWidget, required=False)
     audience = forms.ModelChoiceField(queryset=Audience.objects.all())
     theme = forms.ModelMultipleChoiceField(queryset=Theme.objects.all(), widget=Select2MultipleWidget, required=False)
-    imageURL = forms.CharField(max_length=300)
+    image = forms.ImageField(required=False)
+    x = forms.FloatField(widget=forms.HiddenInput(),required=False)
+    y = forms.FloatField(widget=forms.HiddenInput(), required=False)
+    width = forms.FloatField(widget=forms.HiddenInput(),required=False)
+    height = forms.FloatField(widget=forms.HiddenInput(), required=False)
     resource_DOI = forms.CharField(max_length=100)
     year_of_publication = forms.IntegerField()
 
     class Meta:
         model = Resource
-        fields = ["name", "abstract", "url", "audience", "theme",
-         "keywords", "license", "publisher", "category", "authors","author_email",
-         "imageURL", "resource_DOI", "year_of_publication"]
+        fields = ["name", "abstract", "url", "audience", "theme","keywords", "license", "publisher",
+         "category", "authors","author_email", "image", "x", "y", "width", "height", "resource_DOI", "year_of_publication"]
         
         
 
-    def save(self, args):
+    def save(self, args, photo):
         publication_date = datetime.now()
         rsc = super(ResourceForm, self).save(commit=False)
         category = get_object_or_404(Category, id=self.data['categorySelected'])
@@ -52,10 +56,13 @@ class ResourceForm(forms.ModelForm):
 
         rsc.inLanguage = self.data['language']        
         rsc.author_email = self.data['author_email']
-        rsc.imageURL = self.data['imageURL']
         rsc.resourceDOI = self.data['resource_DOI']
         rsc.datePublished = self.data['year_of_publication']
         rsc.category = category
+
+        if(photo != '/'):
+            rsc.image = photo
+
         rsc.save()
 
         rsc.theme.set(self.data.getlist('theme'))
