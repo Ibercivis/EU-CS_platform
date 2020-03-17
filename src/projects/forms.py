80d31600c327b432324735ec6c4efdbb48c612bf
Francisco Sanz <frasanz@bifi.es>
2020-03-17 18:21:04 +0100

    Working in style

--- src/projects/forms.py
+++ src/projects/forms.py
@@ -21,27 +21,27 @@ class ProjectForm(forms.Form):
     keywords = forms.MultipleChoiceField(choices=(), widget=Select2MultipleWidget, required=False,label="Keywords (Select or write a new ones, comma separated)")
     status = forms.ModelChoiceField(queryset=Status.objects.all())
     start_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=False)
-    end_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=False)  
+    end_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=False)
     topic = forms.ModelMultipleChoiceField(queryset=Topic.objects.all(), widget=Select2MultipleWidget, required=False,label="Topic (Multiple selection)")
     url = forms.CharField(max_length=200, required=False)
     #Contact person info
     contact_person = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'autocomplete':'nope'}))
     contact_person_email = forms.CharField(max_length=100)
     contact_person_phone = forms.CharField(max_length=100, required=False)
-    #Images and communications    
-    image1 = forms.ImageField(required=False,label="Image 1 (Will be resized to 400x300 pixels)")
+    #Images and communications
+    image1 = forms.ImageField(required=False,label="Image 1 (Will be resized to 600x400 pixels)")
     x1 = forms.FloatField(widget=forms.HiddenInput(),required=False)
     y1 = forms.FloatField(widget=forms.HiddenInput(), required=False)
     width1 = forms.FloatField(widget=forms.HiddenInput(),required=False)
     height1 = forms.FloatField(widget=forms.HiddenInput(), required=False)
     image_credit1 = forms.CharField(max_length=200, required=False, label="Image 1 credit")
-    image2 = forms.ImageField(required=False, label="Image 2: Logo (Will be resized to 400x300 pixels)")
+    image2 = forms.ImageField(required=False, label="Image 2: Logo (Will be resized to 600x400 pixels)")
     x2 = forms.FloatField(widget=forms.HiddenInput(),required=False)
     y2 = forms.FloatField(widget=forms.HiddenInput(), required=False)
     width2 = forms.FloatField(widget=forms.HiddenInput(),required=False)
     height2 = forms.FloatField(widget=forms.HiddenInput(), required=False)
     image_credit2 = forms.CharField(max_length=200, required=False, label="Logo credit")
-    image3 = forms.ImageField(required=False, label="Image 3: Header (Will be resized to 1100x300 pixels" )
+    image3 = forms.ImageField(required=False, label="Image 3: Header (Will be resized to 1100x400 pixels" )
     x3 = forms.FloatField(widget=forms.HiddenInput(),required=False)
     y3 = forms.FloatField(widget=forms.HiddenInput(), required=False)
     width3 = forms.FloatField(widget=forms.HiddenInput(),required=False)
@@ -58,21 +58,21 @@ class ProjectForm(forms.Form):
     #Funding
     funding_body =  forms.ModelMultipleChoiceField(queryset=FundingBody.objects.all(), widget=Select2MultipleWidget, required=False, label="Funding bodies (Select or write new one)")
     fundingBodySelected = forms.CharField(widget=forms.HiddenInput(), max_length=100, required=False)
-    funding_program = forms.CharField(max_length=500, required=False)    
+    funding_program = forms.CharField(max_length=500, required=False)
     funding_agency =   forms.ModelMultipleChoiceField(queryset=FundingAgency.objects.all(), widget=Select2MultipleWidget, required=False, label="Funding agency (Select or write new one)")
     fundingAgencySelected = forms.CharField(widget=forms.HiddenInput(), max_length=100, required=False)
 
     def clean(self):
         start_date = self.data['start_date']
-        end_date = self.data['end_date'] 
+        end_date = self.data['end_date']
         if start_date is not '' and end_date is not '' and end_date < start_date:
-            msg = u"End date should be greater than start date."            
+            msg = u"End date should be greater than start date."
             self._errors["end_date"] = self.error_class([msg])
 
     def save(self, args, images):
-        pk = self.data.get('projectID', '') 
+        pk = self.data.get('projectID', '')
         start_dateData = self.data['start_date']
-        end_dateData = self.data['end_date']                             
+        end_dateData = self.data['end_date']
         latitude = self.data['latitude']
         longitude = self.data['longitude']
         country = getCountryCode(latitude,longitude).upper()
@@ -80,7 +80,7 @@ class ProjectForm(forms.Form):
 
         if(pk):
             project = get_object_or_404(Project, id=pk)
-            project.name = self.data['project_name']           
+            project.name = self.data['project_name']
             project.url = self.data['url']
             project.author = self.data['contact_person']
             project.author_email = self.data['contact_person_email']
@@ -88,7 +88,7 @@ class ProjectForm(forms.Form):
             project.longitude = longitude
             project.country = country
             project.aim = self.data['aim']
-            project.description = self.data['description']         
+            project.description = self.data['description']
             project.status = status
             project.host = self.data['host']
             project.imageCredit1 = self.data['image_credit1']
@@ -96,13 +96,13 @@ class ProjectForm(forms.Form):
             project.imageCredit3 = self.data['image_credit3']
             project.howToParticipate = self.data['how_to_participate']
             project.equipment = self.data['equipment']
-            project.fundingProgram = self.data['funding_program']        
-        else:           
+            project.fundingProgram = self.data['funding_program']
+        else:
             project = Project(name = self.data['project_name'],
                          url = self.data['url'], creator=args.user,
                          author = self.data['contact_person'], author_email = self.data['contact_person_email'],
                          latitude = latitude, longitude = longitude, country = country,
-                         aim = self.data['aim'], description = self.data['description'], 
+                         aim = self.data['aim'], description = self.data['description'],
                          status = status, host = self.data['host'], imageCredit1 = self.data['image_credit1'],
                          imageCredit2 = self.data['image_credit2'], imageCredit3 = self.data['image_credit3'],
                          howToParticipate = self.data['how_to_participate'],
@@ -122,30 +122,30 @@ class ProjectForm(forms.Form):
             agency, exist = FundingAgency.objects.get_or_create(agency=fundingAgencySelected)
             project.fundingAgency = agency
 
-                 
+
         if(images[0] != '/'):
             project.image1 = images[0]
-        
+
         if(images[1] != '/'):
             project.image2 = images[1]
-        
+
         if(images[2] != '/'):
             project.image3 = images[2]
-        
+
         project.save()
         project.topic.set(self.data.getlist('topic'))
         choices = self.data['choices']
         choices = choices.split(',')
         for choice in choices:
             if(choice != ''):
-                keyword = Keyword.objects.get_or_create(keyword=choice)                   
+                keyword = Keyword.objects.get_or_create(keyword=choice)
         keywords = Keyword.objects.all()
         keywords = keywords.filter(keyword__in = choices)
         project.keywords.set(keywords)
         return 'success'
 
 
-def getCountryCode(latitude, longitude):       
+def getCountryCode(latitude, longitude):
     location = geolocator.reverse([latitude, longitude], exactly_one=True)
     if len(location.raw) > 1:
         return location.raw['address']['country_code']
