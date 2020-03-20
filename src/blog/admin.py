e3fbb02eb81d8e0bd4572e0dcd1d185657efe595
Victor Val <vval@bifi.es>
2020-03-20 15:50:53 +0100

    Summernote complete for post #117

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,12 +1,51 @@
-from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.urls import reverse
+from django.conf import settings
+from django_summernote.widgets import SummernoteWidget
+from django import forms
 from .models import Post
 
-class PostAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status','created_on')
+
+class SummernoteModelAdminWithCustomToolbar(SummernoteWidget):
+    def summernote_settings(self):
+        summernote_settings = settings.SUMMERNOTE_CONFIG.get(
+            'summernote', {}).copy()
+        lang = settings.SUMMERNOTE_CONFIG['summernote'].get('lang')
+        if not lang:
+            lang = 'en-US'
+        summernote_settings.update({
+            'width': '80%',
+            'height': '500',
+            'lang': lang,
+            'url': {
+                'language': static('summernote/lang/summernote-' + lang + '.min.js'),
+                'upload_attachment': reverse('django_summernote-upload_attachment'),
+            },
+            'toolbar': [
+                ['style', ['style', ]],
+                ['font', ['bold', 'italic', 'underline', 'color', ]],
+                ['paragraph', ['paragraph', 'ol', 'ul', ]],
+                ['misc', ['link', 'picture', 'undo', 'redo', 'help', ]],
+            ],
+
+        })
+        return summernote_settings
+
+
+class CustomPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        widgets = {'content': SummernoteModelAdminWithCustomToolbar()}
+        fields = "__all__"
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
-  
+    form = CustomPostForm
+
+
 admin.site.register(Post, PostAdmin)
