a310216e05298ef212154895c1edaea6c36b0799
Victor Val <vval@bifi.es>
2020-03-30 09:33:45 +0200

    Confirmation of email step in registration #146

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -9,7 +9,16 @@ from django.shortcuts import redirect
 from authtools import views as authviews
 from braces import views as bracesviews
 from django.conf import settings
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.shortcuts import render
+from .tokens import account_activation_token
 from . import forms
+
 
 
 User = get_user_model()
@@ -46,11 +55,23 @@ class SignUpView(
 
     def form_valid(self, form):
         r = super().form_valid(form)
-        username = form.cleaned_data["email"]
-        password = form.cleaned_data["password1"]
-        user = auth.authenticate(email=username, password=password)
-        auth.login(self.request, user)
-        return r
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+        current_site = get_current_site(self.request)
+        mail_subject = 'Activate your EU-Citizen.Science account.'
+        message = render_to_string('accounts/acc_active_email.html', {
+            'user': user,
+            'domain': current_site.domain,
+            'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+            'token':account_activation_token.make_token(user),
+        })
+        to_email = form.cleaned_data.get('email')
+        email = EmailMessage(
+                    mail_subject, message, to=[to_email]
+        )
+        email.send()
+        return render(self.request, 'accounts/confirm-email.html',{})
 
 
 class PasswordChangeView(authviews.PasswordChangeView):
@@ -98,3 +119,17 @@ def delete_user(request):
             
     return redirect('home')
     
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        auth.login(request, user)
+        return render(request, 'accounts/confirmation-account.html',{})
+    else:
+        return HttpResponse('Activation link is invalid!')
