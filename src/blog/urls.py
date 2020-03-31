2de0e8440f84c97c2b26f142f69a4f9851557115
Victor Val <vval@bifi.es>
2020-03-31 15:20:11 +0200

    Added rating to blog

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('blog', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post_detail/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+    path('post_review/<int:pk>', views.post_review, name='post_review'),
 ]
