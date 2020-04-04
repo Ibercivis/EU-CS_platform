672524f629a442063e5fbf449461585c75e37bcb
Francisco Sanz <frasanz@bifi.es>
2020-04-04 18:21:41 +0200

    adding map, not yet ready

--- src/eucs_platform/views.py
+++ src/eucs_platform/views.py
@@ -19,14 +19,17 @@ def home(request):
     lastBlogEntry = Post.objects.all().filter(status=1)
     if lastBlogEntry:
         lastBlogEntry = lastBlogEntry.latest('created_on')
-    featuredProjects = FeaturedProjects.objects.all().order_by('-id').values_list('project_id',flat=True)
+    featuredProjects = FeaturedProjects.objects.all().order_by('-id')[:6].values_list('project_id',flat=True)
+    allfeaturedProjects = FeaturedProjects.objects.all().order_by('-id').values_list('project_id',flat=True)
     projects = Project.objects.all().order_by('-id')
+    allprojects =  projects.filter(id__in=allfeaturedProjects)
     projects = projects.filter(id__in=featuredProjects)
+
     featuredResources = FeaturedResources.objects.all().order_by('-id')[:6].values_list('resource_id',flat=True)
     resources = Resource.objects.all().order_by('-id')
     resources = resources.filter(id__in=featuredResources)
     print(projects)
-    return render(request, 'home.html', {'projects':projects, 'resources':resources, 'lastBlogEntry': lastBlogEntry})
+    return render(request, 'home.html', {'projects':projects, 'allprojects': allprojects,'resources':resources, 'lastBlogEntry': lastBlogEntry})
 
 class AboutPage(generic.TemplateView):
     template_name = "about.html"
