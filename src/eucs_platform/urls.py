c32df1beaf0b3827e72d74da9f74436b3fb21324
Francisco Sanz <frasanz@bifi.es>
2020-04-05 22:07:50 +0200

    Created quality criteria page. Fix #212

--- src/eucs_platform/urls.py
+++ src/eucs_platform/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("faq/", views.faq,name="faq"),
     path("subscribe/", views.subscribe,name="subscribe"),
     path("moderation/", views.moderation,name="moderation"),
+    path("criteria/", views.criteria,name="criteria"),
     path("home_autocomplete/", views.home_autocomplete,name="home_autocomplete"),
     path("development/", views.development,name="development"),
     path("about/", views.AboutPage.as_view(), name="about"),
