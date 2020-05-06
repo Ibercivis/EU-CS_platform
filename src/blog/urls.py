3e1c53e58ee08b0cb6bf33cff814314d9d7ab263
Victor Val <vval@bifi.es>
2020-05-06 12:11:41 +0200

    Show only post detail with status published

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('blog', views.PostList.as_view(), name='blog'),
-    path('blog/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('blog/<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
     path('post_review/<int:pk>', views.post_review, name='post_review'),
 ]
