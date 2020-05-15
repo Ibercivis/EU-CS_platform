8b90059d73127b9539818413a2147dc2430d2571
Victor Val <vval@bifi.es>
2020-05-15 10:09:29 +0200

    place not required in events page

--- src/events/forms.py
+++ src/events/forms.py
@@ -6,7 +6,7 @@ from .models import Event
 class EventForm(forms.Form):
     title = forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder':'Title of the event'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'A little description of the event. Max 3000 characters'}), max_length = 3000)
-    place = forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder':'Place where event happens'}))
+    place = forms.CharField(max_length=200,widget=forms.TextInput(attrs={'placeholder':'Place where event happens'}),required=False)
     start_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     end_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
     url = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'placeholder':'Please provide a URL to an external web site for the event'}),required=False)
