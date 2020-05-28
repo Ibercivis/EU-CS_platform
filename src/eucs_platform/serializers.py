d3fef5451656123c9c7ecccce7607e1fc445bbd8
Victor Val <vval@bifi.es>
2020-05-28 16:22:12 +0200

    Add rest auth

--- src/eucs_platform/serializers.py
+++ src/eucs_platform/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
+from rest_auth.serializers import UserDetailsSerializer
 User = get_user_model()
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(UserDetailsSerializer):
     class Meta:
         model = User
-        fields = ['name', 'email']
+        fields = ['name', 'email', 'password']
 
