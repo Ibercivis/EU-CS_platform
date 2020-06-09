def64690c326fef135d35f6ea00bd7ae994b69b6
Victor Val <vval@bifi.es>
2020-06-09 08:33:27 +0200

    API - Topic and status endpoints for projects

--- src/projects/api/urls.py
+++ src/projects/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'projects/topic', views.TopicViewSet, basename='topic')
+router.register(r'projects/status', views.StatusViewSet, basename='status')
 
 urlpatterns = [
     path('projects/', views.ProjectList.as_view(), name="api_projects"),
     path('projects/<int:pk>', views.ProjectDetail.as_view(), name="api_project_detail"),
 ]
+
+urlpatterns += router.urls
