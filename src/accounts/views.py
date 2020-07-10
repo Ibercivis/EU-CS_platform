49c62e6853e4cf9687cc67a9bd3036b66be0d582
Victor Val <vval@bifi.es>
2020-07-10 15:30:47 +0200

    Correct sender email in signup

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -81,7 +81,7 @@ class SignUpView(
         })
 
         to_email = form.cleaned_data.get('email')
-        send_mail(mail_subject, message, 'eu-citizen.sciece@ibercivis.es',[to_email], html_message=html_message)
+        send_mail(mail_subject, message, 'eu-citizen.science@ibercivis.es',[to_email], html_message=html_message)
 
         return render(self.request, 'accounts/confirm-email.html',{})
 
