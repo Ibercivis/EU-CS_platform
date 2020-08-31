b4b61722deac6a82986e08c48ffb9f5ee1e2daee
Victor Val <vval@bifi.es>
2020-08-31 12:38:46 +0200

    Added country to organisation model

--- src/organisations/models.py
+++ src/organisations/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django_countries.fields import CountryField
 
 class OrganisationType(models.Model):
     type = models.TextField()
@@ -20,7 +21,7 @@ class Organisation(models.Model):
     contactPointEmail = models.CharField(max_length=100, null=True, blank=True)
     latitude = models.DecimalField(max_digits=9,decimal_places=6, null=True, blank=True)
     longitude = models.DecimalField(max_digits=9,decimal_places=6, null=True, blank=True)
-  
+    country = CountryField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.name}'
