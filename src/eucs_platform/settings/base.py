4ed2451536c6c361d2f02c8798459670dd046365
Victor Val <vval@bifi.es>
2020-09-03 13:10:35 +0200

    Change email backend from console to smtp

--- src/eucs_platform/settings/base.py
+++ src/eucs_platform/settings/base.py
@@ -280,8 +280,8 @@ SUMMERNOTE_CONFIG = {
     'disable_attachment': True,
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = env("FROM_EMAIL")
 EMAIL_HOST = env("HOST_EMAIL")
 EMAIL_HOST_USER = env("FROM_EMAIL")
