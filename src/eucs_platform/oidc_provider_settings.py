38a680ee8c37bc7745a90a205726b38ddba8a3dc
Ubuntu <ubuntu@vps-dabc6e43.vps.ovh.net.novalocal>
2021-01-20 10:03:24 +0000

    improved openid connection

--- src/eucs_platform/oidc_provider_settings.py
+++ src/eucs_platform/oidc_provider_settings.py
@@ -2,7 +2,7 @@ def userinfo(claims, user):
     print("userinfo.............")
     print(user)
     # Populate claims dict.
-    claims['name'] = user.name
+    claims['name'] = user.email
     claims['given_name'] = user.name
     claims['family_name'] = user.name
     claims['email'] = user.email
