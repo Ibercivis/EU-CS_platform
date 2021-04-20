40104b8dda0c27956efe450b75daaf4379ceab26
Victor Val <vval@bifi.es>
2021-04-20 10:05:40 +0200

    Config email for testing

--- src/eucs_platform/settings/base.py
+++ src/eucs_platform/settings/base.py
@@ -309,8 +309,8 @@ SUMMERNOTE_CONFIG = {
     'disable_attachment': True,
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = env("FROM_EMAIL")
 EMAIL_HOST = env("HOST_EMAIL")
 EMAIL_HOST_USER = env("FROM_EMAIL")
@@ -327,8 +327,8 @@ EMAIL_RECIPIENT_LIST = [
 EMAIL_CONTACT_RECIPIENT_LIST = [
     "eucitsci@mfn.berlin",
     "frasanz@bifi.es",
-    "Claudia.FaboCartas@mfn.berlin",
-    "Antonella.Radicchi@mfn.berlin"
+    "Claudia.FaboCartas@mfn.berlin"#,
+#    "Antonella.Radicchi@mfn.berlin"
 ]
 
 HOST =  env("HOST")
