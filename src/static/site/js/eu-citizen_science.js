a6f465010d6812420405ee56d2c987b011c37461
Francisco Sanz <frasanz@bifi.es>
2023-04-02 19:29:16 +0200

    Likes in projects

--- src/static/site/js/eu-citizen_science.js
+++ src/static/site/js/eu-citizen_science.js
@@ -1,3 +1,52 @@
+const CardManager = {
+    clickOnHeart: function(project_id){
+        $.ajax({
+            type: 'POST',
+            url: '/likeProjectAjax',
+            data: {project_id: project_id},
+            success: function(response){
+                console.log(response)
+                var hijo = $('#heart'+project_id).find(".value")
+                var valor = parseInt(hijo.text(),10);
+                if(response['Liked'] === "True"){
+                    $('#heart'+project_id).addClass("animate__animated animate__fadeIn text-danger")
+                    $('#heart'+project_id).removeClass("text-muted")
+                    valor ++;
+                    $('#heart'+project_id).one("animationend", function() {
+                        $('#heart'+project_id).removeClass("animate__animated animate__fadeIn ");
+                    });
+                }else{
+                    $('#heart'+project_id).addClass("animate__animated animate__fadeIn text-muted")
+                    $('#heart'+project_id).removeClass("text-danger")
+                    valor--;
+                    $('#heart'+project_id).one("animationend", function() {
+                        $('#heart'+project_id).removeClass("animate__animated animate__fadeIn");
+                    });
+                }
+                hijo.text(valor);
+
+            },
+            error: function(response){
+                console.log(response)
+            },
+        })
+    }
+};
+$(document).ready(function() {
+    function getCSRFToken() {
+        return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            xhr.setRequestHeader('X-CSRFToken', getCSRFToken());
+        }
+    });
+    $(".heart").on("click", function() {
+        // Código que se ejecutará cuando se haga clic en el elemento con el ID "mi-boton"
+        console.log("heart clicked");
+        CardManager.clickOnHeart($(this).attr('project_id'))
+    });
+});
 $(function() {
     $('.doModalAction').click(function(){
         console.log($(this).attr('id'))
@@ -50,3 +99,7 @@ function attatchDeletePlatform(){
         })
     })
 }
+
+
+
+
